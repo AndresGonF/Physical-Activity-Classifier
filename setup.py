2ed2bacff523fbfcc9156b4e4407ae8b3a6ad107
Andres Gonzalez Fuentes <andres.gonzalez.f@ug.uchile.cl>
2021-07-23 00:40:19 -0400

    Initiate repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classification of physical activities from inertial sensor data "UCF-iPhone Dataset"',
+    author='Andres Gonzalez Fuentes',
+    license='MIT',
+)
